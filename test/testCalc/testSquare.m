cd8f7766af83c209bef7d1d6a08e7f5a19f8d6aa
Vahila <70003902+Vahila@users.noreply.github.com>
2021-03-05 10:21:33 +0530

    Updating test cases in testCalc folder

---
+++ test/testCalc/testSquare.m
@@ -0,0 +1,12 @@
+classdef testSquare < matlab.unittest.TestCase
+    %TESTCALC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods(Test)
+        function testSquareForNumbers(testCase)
+            %TESTCALC Construct an instance of this class
+            %   Detailed explanation goes here
+            testCase.verifyEqual(sqNum(2), 4);
+        end
+    end
+end
