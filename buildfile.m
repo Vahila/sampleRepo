a4a5ebbce415193fed9ef29d56c15336b4afbdf0
Vahila Kayithi <vkayithi@mathworks.com>
2024-09-10 09:19:52 +0530

    Update buildfile

--- buildfile.m
+++ buildfile.m
@@ -2,64 +2,31 @@ function plan = buildfile
 % Create a plan from the task functions
 plan = buildplan(localfunctions);
 
-% Make the "archive" task the default task in the plan
-plan.DefaultTasks = "test";
+% Specify the inputs and outputs of the "pcode" task
+plan("pcode").Inputs = "source/**/*.m";
+plan("pcode").Outputs = plan("pcode").Inputs.replace(".m",".p");
 
-% Make the "archive" task dependent on the "check" and "test" tasks
-plan("archive1").Dependencies = ["check1" "test1"];
-plan("archive2").Dependencies = ["check2" "test2"];
+% Specify the inputs and outputs of the "archive" task
+plan("archive").Inputs = plan("pcode").Outputs;
+plan("archive").Outputs = "source.zip";
 end
 
-function check1Task(~)
-% Identify code issues
-% issues = codeIssues;
-% assert(isempty(issues.Issues),formattedDisplayText( ...
-%    issues.Issues(:,["Location" "Severity" "Description"])))
-disp('In check task');
+function pcodeTask(context)
+% Create P-code files
+filePaths = context.Task.Inputs.paths;
+% pcode(filePaths{:},"-inplace")
 end
 
-function test1Task(~)
-% Run unit tests
-% results = runtests(IncludeSubfolders=true,OutputDetail="terse");
-% assertSuccess(results);
+function archiveTask(context)
+% Create ZIP file
+task = context.Task;
+% zip(task.Outputs.paths,task.Inputs.paths)
+end
+
+function testTask(~)
 disp('In test task');
 end
 
-function archive1Task(~)
-% Create ZIP file
-% zipFileName = "source_" + ...
-%     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
-% zip(zipFileName,"*")
-disp('In archive task');
+function errortask(~)
+disp(a);
 end
-
-function check2Task(~)
-% Identify code issues
-% issues = codeIssues;
-% assert(isempty(issues.Issues),formattedDisplayText( ...
-%    issues.Issues(:,["Location" "Severity" "Description"])))
-disp('In check task');
-end
-
-function test2Task(~)
-% Run unit tests
-% results = runtests(IncludeSubfolders=true,OutputDetail="terse");
-% assertSuccess(results);
-disp('In test task');
-end
-
-function archive2Task(~)
-% Create ZIP file
-% zipFileName = "source_" + ...
-%     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
-% zip(zipFileName,"*")
-disp('In archive task');
-end
-
-%function testTask(~)
-% Create ZIP file
-% zipFileName = "source_" + ...
-%     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
-% zip(zipFileName,"*")
-%a=b;
-%end
